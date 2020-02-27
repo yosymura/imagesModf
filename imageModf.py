82072e0a45b54c3083f9e680cc6b94ef14eabbdf
yosymura <yosy-mura@hotmail.com>
2020-02-28 00:40:19 +0100

    copyFunc 1.0

--- imageModf.py
+++ imageModf.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-from typing import Union
 
 from_dir = "D:\\..."
 to_dir = "D:\\..."
@@ -11,11 +10,11 @@ video_ext = ['.MKV','.MP4','.AVI','.WMV','.MOV','.FLV','.AVCHD']
 
 
 def copyFunc(ext,src,dist):
-		for root, dirs, files in os.walk(src):
-			for file in files:
-				if file.endswith(tuple(ext)):
-					file_url = os.path.join(root, file)
-					shutil.copy2(file_url, dist)
-		print("Copy finished!")
+	for root, dirs, files in os.walk(src):
+		for file in files:
+			if file.endswith(tuple(ext)):
+				file_url = os.path.join(root, file)
+				shutil.copy2(file_url, dist)
+	print("Copy finished!")
 
 copyFunc(pic_ext,from_dir,to_dir)
