ff9f39d7e55fc05f2dd090e3e49d812fd9253c6b
yosymura <yosy-mura@hotmail.com>
2020-02-28 00:20:58 +0100

    copyFunc 1.0

--- imageModf.py
+++ imageModf.py
@@ -1,19 +1,20 @@
 import os
 import shutil
+from typing import Union
 
 from_dir = "D:\\..."
 to_dir = "D:\\..."
 
-pic_ext=['.png','.jpeg','.jpg']
-audio_ext=[]
-video_ext= []
+pic_ext = ['.PNG','.JPEG','.JPG']
+audio_ext = ['.MP3','.AAC','.WAV','.FLAC','.ALAC','.DSD']
+video_ext = ['.MKV','.MP4','.AVI','.WMV','.MOV','.FLV','.AVCHD']
 
 
 def copyFunc(ext,src,dist):
 		for root, dirs, files in os.walk(src):
 			for file in files:
 				if file.endswith(tuple(ext)):
-					file_url = os.path.join (root, file)
+					file_url = os.path.join(root, file)
 					shutil.copy2(file_url, dist)
 		print("Copy finished!")
 
