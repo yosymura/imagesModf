6b7dfc5ce50e4410e42c8279424f39f9770b32e5
yosymura <yosy-mura@hotmail.com>
2020-02-27 22:33:59 +0100

    copyFunc 1.0

--- imageModf.py
+++ imageModf.py
@@ -1,6 +1,20 @@
 import os
+import shutil
 
-for root, dirs, files in os.walk('c:\images'):
-	for file in files:
-		if file.endswith('.png'):
-			print(os.path.join(root, file))
+from_dir = "D:\\lofasz"
+to_dir = "D:\\lofasz_dist"
+
+pic_ext=['.png','.jpeg','.jpg']
+audio_ext=[]
+video_ext= []
+
+
+def copyFunc(ext,src,dist):
+		for root, dirs, files in os.walk(src):
+			for file in files:
+				if file.endswith(tuple(ext)):
+					file_url = os.path.join (root, file)
+					shutil.copy2(file_url, dist)
+		print("Copy finished!")
+
+copyFunc(pic_ext,from_dir,to_dir)
